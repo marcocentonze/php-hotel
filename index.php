8e9418b7cb6f79c7f86562cc83f7187950ae736c
marcocentonze <marco.centonze27@gmail.com>
2023-10-10 15:57:08 +0200

    add table styled with bootstrap

--- index.php
+++ index.php
@@ -61,26 +61,55 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
 
+<div class="container">
+<table class="table table-info">
+  <thead>
+    <tr>
+      <th scope="row">Name</th>
+      <?php foreach ($hotels as $hotel) : ?>
+        <th scope="col"><?php echo $hotel['name']; ?></th>
+      <?php endforeach; ?>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Description</th>
+      <?php foreach ($hotels as $hotel) : ?>
+        <td><?php echo $hotel['description']; ?></td>
+      <?php endforeach; ?>
+    </tr>
+    <tr>
+      <th scope="row">Parking</th>
+      <?php foreach ($hotels as $hotel) : ?>
+        <td><?php echo $hotel['parking'] ? 'Disponibile' : 'Non disponibile'; ?></td>            
+      <?php endforeach; ?>
+    </tr>
+    <tr>
+      <th scope="row">Vote</th>
+      <?php foreach ($hotels as $hotel) : ?>
+        <td><?php echo $hotel['vote']; ?></td>
+      <?php endforeach; ?>
+    </tr>
+    <tr>
+      <th scope="row">Distance to center</th>
+      <?php foreach ($hotels as $hotel) : ?>
+        <td><?php echo $hotel['distance_to_center']; ?> km</td>
+      <?php endforeach; ?>
+    </tr>
+  </tbody>
+</table>
+</div>
 
 
-    <?php
-    foreach ($hotels as $hotel) {
-        echo "Nome: " . $hotel['name'] . "<br>";
-        echo "Descrizione: " . $hotel['description'] . "<br>";
-        echo "Parcheggio: " . ($hotel['parking'] ? 'Disponibile' : 'Non disponibile') . "<br>";
-        echo "Voto: " . $hotel['vote'] . "<br>";
-        echo "Distanza dal centro: " . $hotel['distance_to_center'] . " km<br><br>";
-    }
-    ?>
 
 
-
-
-
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 
 </html>
