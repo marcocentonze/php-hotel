abaab2e3b3f5859b0924c2eb5295efc1062e90f9
marcocentonze <marco.centonze27@gmail.com>
2023-10-10 15:37:38 +0200

    print data in page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // Descrizione
 // Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
@@ -8,23 +8,79 @@
 //  Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
 // Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
 
+//arrya di hotel
+$hotels = [
 
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+];
 
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+
+    <?php
+    foreach ($hotels as $hotel) {
+        echo "Nome: " . $hotel['name'] . "<br>";
+        echo "Descrizione: " . $hotel['description'] . "<br>";
+        echo "Parcheggio: " . ($hotel['parking'] ? 'Disponibile' : 'Non disponibile') . "<br>";
+        echo "Voto: " . $hotel['vote'] . "<br>";
+        echo "Distanza dal centro: " . $hotel['distance_to_center'] . " km<br><br>";
+    }
+    ?>
+
+
+
+
+
+</body>
+
+</html>
